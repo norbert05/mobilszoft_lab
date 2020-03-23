43486d45c1e860607fb0f27886b5c5b24715123e
Horvath Norbert <horvath.norbert@autsoft.hu>
2020-03-23 09:51:17 +0100

    adds newsList architecture elements

---
+++ MobSoftHw/app/src/main/java/com/example/mobsofthw/scenes/newsList/NewsListInteractor.kt
@@ -0,0 +1,4 @@
+package com.example.mobsofthw.scenes.newsList
+
+class NewsListInteractor {
+}
---
+++ MobSoftHw/app/src/main/java/com/example/mobsofthw/scenes/newsList/NewsListPresenterInput.kt
@@ -0,0 +1,4 @@
+package com.example.mobsofthw.scenes.newsList
+
+interface NewsListPresenterInput {
+}
